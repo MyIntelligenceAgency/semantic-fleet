c2e5250c404246cb20933c189d3eb530fa1c3ece
Jean-Sylvain Boige <jsboige@gmail.com>
2023-09-20 11:36:28 +0200

    Introducing Notebooks

---
+++ dotnet/notebooks/config/OobaboogaConnectorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) MyIA. All rights reserved.
+
+
+/// <summary>
+/// Represents a text completion provider instance with the corresponding given name and settings.
+/// </summary>
+public class OobaboogaConnectorConfiguration
+{
+    public string? EndPoint { get; set; } = "http://localhost";
+
+    public int BlockingPort { get; set; } = 5000;
+
+    public int StreamingPort { get; set; } = 5005;
+
+}
---
+++ dotnet/notebooks/config/OpenAIConfiguration.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) MyIA. All rights reserved.
+
+public class OpenAIConfiguration
+{
+    public string TextModelId { get; set; } = "text-davinci-003";
+    public string? ChatModelId { get; set; } = "gpt-3.5-turbo";
+    public string ApiKey { get; set; }
+
+
+}
